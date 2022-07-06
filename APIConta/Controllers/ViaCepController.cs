d3eb546ab4983164e660ded983202c023bbfba56
Marcelo <guedesbr@hotmail.com>
2022-07-06 13:29:15 -0300

    Commit inicial

---
+++ APIConta/Controllers/ViaCepController.cs
@@ -0,0 +1,27 @@
+﻿using APIConta.Model;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+
+namespace APIConta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViaCepController : ControllerBase
+    {
+        // GET: api/<ViaCepController>
+        [HttpGet]
+        public ViaCep Get()
+        {
+            var client = new RestClient("http://viacep.com.br");
+            var request = new RestRequest("ws/:cep/json/", Method.Get);
+            request.AddHeader("Accept", "text/json");
+            request.AddParameter("cep", "01001000", ParameterType.UrlSegment);
+ 
+            var queryResult = client.Execute<ViaCep>(request).Data;
+
+            return queryResult;
+        }
+
+    }
+}
